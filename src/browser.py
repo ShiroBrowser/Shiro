8813277c1aab8cc4598eb8662d7cefd5468fd229
Astro <astrochanneloficial@gmail.com>
2026-02-24 19:46:15 -0300

    Add initial setup for Searcher class in browser.py

--- src/browser.py
+++ src/browser.py
@@ -1,1 +1,11 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import QUrl
 
+
+class Searcher:
+    def search(self):
+        url = self.url_bar_text()
+
+        return url.search()
